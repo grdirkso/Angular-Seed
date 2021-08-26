c53d9d8271c7ce00eaf9ff79b0d828cd1c9f6bb4
grdirkso <grdirkso@uark.edu>
2021-08-26 09:24:14 -0500

    creating a form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ChildComponent } from './child/child.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent, HeaderComponent, ChildComponent ],
-    bootstrap:    [ AppComponent ]})
+    imports:      [ 
+        BrowserModule,
+        FormsModule 
+    ],
+    declarations: [ 
+        AppComponent, 
+        HeaderComponent, 
+        ChildComponent, 
+        LoginComponent 
+    ],
+    bootstrap: [ AppComponent ]})
 export class AppModule {}
